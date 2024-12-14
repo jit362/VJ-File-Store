c26c0bc3b7ab94beed953f7b5ccdb5554e6cdbbf
Jit <142555479+jit362@users.noreply.github.com>
2024-12-14 06:05:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -222,12 +222,14 @@ async def start(client, message):
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         if not await check_verification(client, message.from_user.id) and VERIFY_MODE == True:
             btn = [[
-                InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{username}?start="))
+                InlineKeyboardButton("❤️‍🔥➖️➖️ Verify Now ➖️➖️❤️‍🔥", url=await get_token(client, message.from_user.id, f"https://telegram.me/{username}?start="))
             ],[
-                InlineKeyboardButton("How To Open Link & Verify", url=VERIFY_TUTORIAL)
+                InlineKeyboardButton("‼️ How To Open Link & Verify ⁉️", url=VERIFY_TUTORIAL)
             ]]
             await message.reply_text(
-                text="<b>❌️ You are not verified !\n 😇 Kindly verify to continue ! 👇</b>",
+                text="<b>❌️ You are not verified ! For Today ❌️ \n\n 
+😇 Kindly verify to continue ! 👇 \n\n 
+‼️ Verify Once In a Day and get all files Direct full day. ✅️</b>",
                 protect_content=True,
                 reply_markup=InlineKeyboardMarkup(btn)
             )
